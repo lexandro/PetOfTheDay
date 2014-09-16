19492d33814ae8503541fdd0e03bb902c5a99f22
lexandro <norton>
2014-09-16 23:33:55 +0200

    added few buttons

--- app/src/main/java/potd/zooplus/com/petoftheday/main.java
+++ app/src/main/java/potd/zooplus/com/petoftheday/main.java
@@ -4,17 +4,48 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
+import com.squareup.picasso.Picasso;
 
-public class main extends Activity {
+
+public class Main extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ImageView imageView = (ImageView) findViewById(R.id.petPicture);
+        Picasso.with(getApplicationContext()).load("http://www.tarotfinal.com/wp-content/uploads/2013/03/dudas.jpg").into(imageView);
 
+
+        //
+        Button likeButton = (Button) findViewById(R.id.like);
+        Button dislikeButton = (Button) findViewById(R.id.dislike);
+
+//        likeButton.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                Toast.makeText(getApplicationContext(), "Like pressed", Toast.LENGTH_SHORT);
+//            }
+//        });
+//
+//        dislikeButton.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                Toast.makeText(getApplicationContext(), "Dislike pressed", Toast.LENGTH_SHORT);
+//            }
+//        });
+
+    }
+
+    public void likeClicked(View view) {
+    }
+
+    public void dislikeClicked(View view) {
+    }
+
+    public void takePhoto(View view) {
     }
 
 
